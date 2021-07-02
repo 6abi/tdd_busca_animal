37cfb40028df80e9c1a9e31967d5bb42d1e4e895
Bárbara <6abi.cardoso@gmail.com>
2021-07-01 22:40:17 -0300

    Criando o teste de roda da pagina index

--- setup/tests.py
+++ setup/tests.py
@@ -8,10 +8,9 @@ class AnimaisTestCase(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-        
-    def test_abre_janela_do_chrome(self):
-        self.browser.get(self.live_server_url)
-        
-    def test_deu_ruim(self):
-        """Teste de exemplo de erro"""
-        self.fail("Teste falhou - deu ruim tio")
+
+    def test_buscanco_um_novo_animal(self):
+        """Teste se um usuário encontra um animal pesquisando"""
+        home_page = self.browser.get(self.live_server_url + '/')
+        brand_element = self.browser.find_element_by_css_selector('.navbar')
+        self.assertEqual('Busca animal', brand_element.text)
